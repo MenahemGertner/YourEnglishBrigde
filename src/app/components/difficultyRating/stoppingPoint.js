24578e3f410a660fa8d6fddeeb93404417f74f42
Menahem Gertner <meni1547@gmail.com>
2025-01-12 09:59:38 +0200

    הוספת קומפוננטת דירוג קושי

---
+++ src/app/components/difficultyRating/stoppingPoint.js
@@ -0,0 +1,11 @@
+import {Cat} from 'lucide-react';
+import Link from 'next/link';
+
+const StoppingPoint = () => {
+    return(
+        <div className='h-14 w-14 text-blue-900 hover:text-blue-700 '>
+        <Cat/>
+        </div>
+    )
+}
+export default StoppingPoint
