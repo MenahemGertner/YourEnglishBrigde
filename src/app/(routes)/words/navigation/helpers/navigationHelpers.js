1cfe6296264b7f292cdc9c900ecda242a4c0528b
Menahem Gertner <meni1547@gmail.com>
2025-06-19 21:32:00 +0300

    mathc to full lists

--- src/app/(routes)/words/navigation/helpers/navigationHelpers.js
+++ src/app/(routes)/words/navigation/helpers/navigationHelpers.js
@@ -1,15 +1,16 @@
-// כאשר הרשימות מלאות, ניתן להחליף בקוד הזה, כדי למנוע קריאת API מיותרת לגודל הרשימה.
-// const CATEGORY_SIZE = 300;
+const CATEGORY_SIZE = 300;
 
-// export function getAdjacentIndex(currentIndex, direction) {
-//   const categoryNumber = Math.ceil(currentIndex / CATEGORY_SIZE);
-//   const categoryStart = (categoryNumber - 1) * CATEGORY_SIZE + 1;
-//   const categoryEnd = categoryStart + CATEGORY_SIZE;
+export function getAdjacentIndex(currentIndex, direction) {
+  const categoryNumber = Math.ceil(currentIndex / CATEGORY_SIZE);
+  const categoryStart = (categoryNumber - 1) * CATEGORY_SIZE + 1;
+  const categoryEnd = categoryStart + CATEGORY_SIZE;
 
-export function getAdjacentIndex(currentIndex, direction, categorySize) {
-    const categoryNumber = Math.ceil(currentIndex / 300);
-    const categoryStart = (categoryNumber - 1) * 300 + 1;
-    const categoryEnd = categoryStart + categorySize - 1;
+  // אם הרשימות לא מלאות, ניתן להחליף לקוד הזה
+
+// export function getAdjacentIndex(currentIndex, direction, categorySize) {
+//     const categoryNumber = Math.ceil(currentIndex / 300);
+//     const categoryStart = (categoryNumber - 1) * 300 + 1;
+//     const categoryEnd = categoryStart + categorySize - 1;
   
     if (direction === 'next') {
       return currentIndex === categoryEnd ? null : currentIndex + 1;
